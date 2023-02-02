9fee5b0acba5063d4611009704e783027b469b8a
vasavi-maddi <maddivasavi19@gmail.com>
2023-02-02 10:11:27 +0530

    updated

--- Mentor/assets/js/sorting.js
+++ Mentor/assets/js/sorting.js
@@ -1,51 +1,67 @@
-let data, table, sortCol;
-let sortAsc = false;
+th = document.getElementsByTagName('th');
 
-async function init() {
-  
-    // Select the table (well, tbody)
-    table = document.querySelector('#myTable');
-    // get the cats
-      let resp = await fetch(document.addEventListener('DOMContentLoaded', init, false);
-  
-      let data, table, sortCol;
-      let sortAsc = false;
-      const pageSize = 3;
-      let curPage = 1;
-      
-      async function init() {
-        
-        // Select the table (well, tbody)
-        table = document.querySelector('#myTable');
-        // get the cats
-        let resp = await fetch(document.getElementById('#mytable'));
-        data = await resp.json();
-        renderTable();
+for(let c=0; c < th.length; c++){
+
+    th[c].addEventListener('click',item(c))
+}
+function item(c){
+
+    return function(){
+      console.log(c)
+      sortTable(c)
+    }
+}
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("myTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
         }
-document.querySelectorAll('#myTable thead tr th').forEach(t => {
-	t.addEventListener('click', sort, false);
-});
-document.querySelectorAll('#myTable thead tr th').forEach(t => {
-	t.addEventListener('click', sort, false);
-});
-function renderTable() {
-    // create html
-    let result = '';
-    data.forEach(c => {
-       result += `<tr>
-       <td>${c.topicname}</td>
-       </tr>`;
-    });
-    table.innerHTML = result;
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
   }
-  function sort(e) {
-    let thisSort = e.target.dataset.sort;
-    if(sortCol === thisSort) sortAsc = !sortAsc;
-    sortCol = thisSort;
-    data.sort((a, b) => {
-      if(a[sortCol] < b[sortCol]) return sortAsc?1:-1;
-      if(a[sortCol] > b[sortCol]) return sortAsc?-1:1;
-      return 0;
-    });
-    renderTable();
-  }
+}
